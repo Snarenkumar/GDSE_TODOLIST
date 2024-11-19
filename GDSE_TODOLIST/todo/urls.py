7f4f2657c86a16a5dfdb48a0b9ea8a8b79040179
S Naren Kumar <vssnaren@gmail.com>
2024-11-19 09:53:18 +0530

    everything is ready to host

--- GDSE_TODOLIST/todo/urls.py
+++ GDSE_TODOLIST/todo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home ,name='home'),
-    path('store',views.store ,name='store'),
+    path('product',views.product ,name='store'),
     
 ]
--- GDSE_TODOLIST/todo/views.py
+++ GDSE_TODOLIST/todo/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'index.html')
+    return render (request,'index.html') 
 
 
-def store(request):
-    return render(request, 'index.html')
+def product(request):
+    
+    listitems=request.GET["to-do"]
+    print(listitems)
+    return render (request,'base.html',{'items': listitems}) 
